b9a93dc29a90c447808b8d33ad71ab0f92360ef6
Christos Papaioannou <hr.papaioannou@gmail.com>
2014-06-28 15:17:59 +0300

    Added materials refractive indices

---
+++ src/Material.cpp
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) 2014 Christos Papaioannou
+ *
+ * This software is provided 'as-is', without any express or implied
+ * warranty. In no event will the authors be held liable for any damages
+ * arising from the use of this software.
+ *
+ * Permission is granted to anyone to use this software for any purpose,
+ * including commercial applications, and to alter it and redistribute it
+ * freely, subject to the following restrictions:
+ *
+ * 1. The origin of this software must not be misrepresented; you must not
+ * claim that you wrote the original software. If you use this software
+ * in a product, an acknowledgment in the product documentation would be
+ * appreciated but is not required.
+ *
+ * 2. Altered source versions must be plainly marked as such, and must not be
+ * misrepresented as being the original software.
+ *
+ * 3. This notice may not be removed or altered from any source distribution.
+ */
+#include "Material.h"
+
+const float REFRACTIVE_INDEX_VACUUM        = 1.0f;
+const float REFRACTIVE_INDEX_AIR           = 1.0002f;
+const float REFRACTIVE_INDEX_LIQUID_HELIUM = 1.025f;
+const float REFRACTIVE_INDEX_ICE		   = 1.31f;          
+const float REFRACTIVE_INDEX_WATER		   = 1.333f;
+const float REFRACTIVE_INDEX_CORNEA        = 1.38f;
+const float REFRACTIVE_INDEX_GLASS_IMPURE  = 1.65f;
+const float REFRACTIVE_INDEX_GLASS_PURE    = 1.52f;
+const float REFRACTIVE_INDEX_DIAMOND       = 2.419f;
