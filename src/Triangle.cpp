50e249c72bc4db6a7c1dca9d48f3d4bec0bb7015
Christos Papaioannou <hr.papaioannou@gmail.com>
2014-10-23 18:15:07 +0300

    Fixed Ray triangle intersection test

--- src/Triangle.cpp
+++ src/Triangle.cpp
@@ -53,42 +53,38 @@ bool Triangle::hit(const Ray& ray, RayIntersection& intersection, float& distanc
 
 bool Triangle::RayTriangleIntersection(const glm::vec3& v1, const glm::vec3& v2, const glm::vec3& v3, const Ray& ray, glm::vec3& barycetricCoords){
 
-   glm::vec3 e1, e2;  //Edge1, Edge2
-   glm::vec3 P, Q, T;
-   float det, inv_det, u, v;
-   float t;
+	float t,u,v;
+	glm::vec3 e1 = v2 - v1;
+	glm::vec3 e2 = v3 - v1;
+	glm::vec3 P = glm::cross(ray.getDirection(), e2);
+	
+	float det = glm::dot(e1, P);
+	if (det > -0.00001f && det < 0.00001f)
+    	return false;
+
+    float inv_det = 1.0f / det;
  
-   //Find vectors for two edges sharing V1
-   e1  = v2 - v1;
-   e2 =  v3 -  v1;
-  
-   P = glm::cross(ray.getDirection(), e2);
-  
-   det = glm::dot(e1, P);
+  	glm::vec3 T = ray.getOrigin() - v1;
+  	glm::vec3 Q = glm::cross(T, e1);
+  	
+  	t = glm::dot(e2, Q) * inv_det;
+	
 
-   // culling
-   if( det < 1e-6) return false;
+  	if (t < 0.0f)
+    	return false;
+  	
+  	u = glm::dot(T, P) * inv_det;
+  	if (u < 0.0f || u > 1.0f)
+    	return false;
 
-   T = ray.getOrigin() - v1;
-   u = glm::dot( T, P);
-   if( u < 0.0 || u > det) return false;
+  	v = glm::dot(ray.getDirection(), Q) * inv_det;
+  	if (v < 0.0f || u + v > 1.0f)
+    	return false;
 
-   Q = glm::cross(T, e1);
-   v = glm::dot( ray.getDirection(), Q);
-   if( v < 0.0 || u + v > det) return false;
-
-   t = glm::dot(e2, Q);
-   inv_det = 1.0 / (float)det;
-
-   t *= inv_det;
-   u *= inv_det;
-   v *= inv_det;
-
-   barycetricCoords.x = t;
-   barycetricCoords.y = u;
-   barycetricCoords.z = v;
-
-   return true;
+   	barycetricCoords.x = t;
+   	barycetricCoords.y = u;
+	barycetricCoords.z = v;
+  	return true;
 }
 
 
