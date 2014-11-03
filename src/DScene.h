0ef6e30d4ca9ea60a2ee92905af6e2d64ed69f2d
Christos Papaioannou <hr.papaioannou@gmail.com>
2014-11-03 20:03:15 +0200

    Added Scene class for Device

---
+++ src/DScene.h
@@ -0,0 +1,58 @@
+/*
+ * Copyright (c) 2014 Christos Papaioannou
+ *
+ * This software is provided 'as-is', without any express or implied
+ * warranty. In no event will the authors be held liable for any damages
+ * arising from the use of this software.
+ *
+ * Permission is granted to anyone to use this software for any purpose,
+ * including commercial applications, and to alter it and redistribute it
+ * freely, subject to the following restrictions:
+ *
+ * 1. The origin of this software must not be misrepresented; you must not
+ * claim that you wrote the original software. If you use this software
+ * in a product, an acknowledgment in the product documentation would be
+ * appreciated but is not required.
+ *
+ * 2. Altered source versions must be plainly marked as such, and must not be
+ * misrepresented as being the original software.
+ *
+ * 3. This notice may not be removed or altered from any source distribution.
+ */
+
+
+#ifndef _DSCENE_H
+#define _DSCENE_H
+
+#include "cudaQualifiers.h"
+#include "DTriangle.h"
+#include "DMaterial.h"
+#include "DLightSource.h"
+
+class DeviceSceneImporter;
+
+class DScene{
+	friend class DeviceSceneImporter;
+
+private:
+	HOST DEVICE DScene():m_Triangles(0),m_NumTriangles(0),m_Lights(0),m_NumLights(0){}
+
+public:
+	
+	DEVICE void setNumTriangles(int num) { m_NumTriangles = num;}
+	DEVICE void setTrianglesArray(DTriangle* array){m_Triangles = array;}
+
+	DEVICE DTriangle* getTriangle(int index)const {return &(m_Triangles[index]);}
+	DEVICE int getNumTriangles()const			{return m_NumTriangles;}
+
+	DEVICE const DLightSource& getLightSource(int index)	{ return m_Lights[index];}
+	DEVICE int getNumLights()	{return m_NumLights;}
+private:
+	DTriangle* m_Triangles;
+	int m_NumTriangles;
+
+	DLightSource* m_Lights;
+	int m_NumLights;
+
+};
+#endif
